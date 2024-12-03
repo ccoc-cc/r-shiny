10ada6700a7919b2e283f9c672c005b17f2da8bb
ccoc <jjl@j-jl.com>
2024-12-03 20:05:51 +0800

    10只看了一点点

---
+++ 09.上传和下载/01.文件输入.R
@@ -0,0 +1,8 @@
+ui <- fluidPage(
+  fileInput("upload", NULL, buttonLabel = "Upload...", multiple = TRUE),
+  tableOutput("files")
+)
+server <- function(input, output, session) {
+  output$files <- renderTable(input$upload)
+}
+shinyApp(ui,server)
